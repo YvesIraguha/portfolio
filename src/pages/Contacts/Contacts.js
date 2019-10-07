e3fb46ecb05e6b1e5181f57a13fd86a9c0178956
YvesIraguha <iraguhaivos@gmail.com>
2019-10-07 19:05:34 +0200

    contact: build contact page - create the content for the contact page
    Starts contact page

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -3,16 +3,19 @@ import "./Contacts.css";
 export default () => (
   <div className="hero is-fullheight">
     <div className="hero-body">
-      <div className="container v-centered">
-        <div className="circle"></div>
-        <div className="rectangle"></div>
-        <div className="circle"></div>
-        <div className="rectangle"></div>
-        <div className="circle"></div>
-        <div className="rectangle"></div>
-        <div className="circle"></div>
-        <div className="rectangle"></div>
-        <p>Contact</p>
+      <div className="container">
+        <p>
+          <i className="fab fa-github"></i>
+          <span className="subtitle is-4">Kigali, Rwanda</span>
+        </p>
+        <p>
+          <i className="fab fa-github"></i>
+          <span className="subtitle is-4">iraguhaivos@gmail.com</span>
+        </p>
+        <p>
+          <i className="fab fa-github"></i>
+          <span className="subtitle is-4">+250-786-338-635</span>
+        </p>
       </div>
     </div>
   </div>
