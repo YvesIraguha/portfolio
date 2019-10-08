f5af04d9e0dc53c0ee8ea563272dc1d79872ee44
YvesIraguha <iraguhaivos@gmail.com>
2019-10-08 19:29:36 +0200

    finish the contacts page - add correct icons - Center the content Finish the page

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -4,18 +4,22 @@ export default () => (
   <div className="hero is-fullheight">
     <div className="hero-body">
       <div className="container">
-        <p>
-          <i className="fab fa-github"></i>
-          <span className="subtitle is-4">Kigali, Rwanda</span>
-        </p>
-        <p>
-          <i className="fab fa-github"></i>
-          <span className="subtitle is-4">iraguhaivos@gmail.com</span>
-        </p>
-        <p>
-          <i className="fab fa-github"></i>
-          <span className="subtitle is-4">+250-786-338-635</span>
-        </p>
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-narrow">
+            <p>
+              <i className="fa fa-map-marker-alt"></i>
+              <span className="subtitle is-4">Kigali, Rwanda</span>
+            </p>
+            <p>
+              <i className="fa fa-envelope"></i>
+              <span className="subtitle is-4">iraguhaivos@gmail.com</span>
+            </p>
+            <p>
+              <i className="fa fa-phone-alt"></i>
+              <span className="subtitle is-4">+250-786-338-635</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
